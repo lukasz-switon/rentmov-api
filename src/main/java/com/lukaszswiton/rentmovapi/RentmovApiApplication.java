2994e2342694f84d0290cfe174086eb319359c1a
Łukasz Świtoń <lukas06.04.93@gmail.com>
2022-08-27 10:56:51 +0200

    Initial commit

---
+++ src/main/java/com/lukaszswiton/rentmovapi/RentmovApiApplication.java
@@ -0,0 +1,13 @@
+package com.lukaszswiton.rentmovapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RentmovApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RentmovApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lukaszswiton/rentmovapi/RentmovApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lukaszswiton.rentmovapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RentmovApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
